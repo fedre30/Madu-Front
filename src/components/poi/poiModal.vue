67c7c42f35035b690f0a198f1da02e94b288bdaf
fedre30 <alfanofederica95@gmail.com>
2020-02-21 12:32:17 +0100

    Fix on modal

--- src/components/poi/poiModal.vue
+++ src/components/poi/poiModal.vue
@@ -157,10 +157,12 @@ export default {
 
   mounted: function() {
     this.isEdit ? (this.formData = this.shop) : this.formData;
+    if (this.isEdit) {
+      this.formData.tags = this.formData.tags.join(", ");
+    }
   },
 
   updated: function() {
-    // eslint-disable-next-line no-console
     this.isEdit ? (this.formData = this.shop) : this.formData;
   },
 
